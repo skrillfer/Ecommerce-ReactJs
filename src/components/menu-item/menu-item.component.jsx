cf989344fdd93579fae6e98b3eb37b23ab81f19c
fernando <rluis4490@gmail.com>
2019-08-05 16:43:27 -0600

    fixed item style

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
+
 import './menu-item.styles.scss';
 
-
-const MenuItem = ({title,imageUrl,size})=>(
-    <div className={`${size} menu-item`}>
-        <div className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }} 
-        >
-            <div className='content'>
-            <div className='title'>{title.toUpperCase()}</div>
-            <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-        
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
+  </div>
 );
 
 export default MenuItem;
-    
+
