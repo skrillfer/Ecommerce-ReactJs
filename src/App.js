cdb6bce303d6d6b558e520a969d0d775b4ff617f
fernando <rluis4490@gmail.com>
2019-08-05 17:09:09 -0600

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component.jsx';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path="/" component={HomePage}/>
     </div>
   );
 }
