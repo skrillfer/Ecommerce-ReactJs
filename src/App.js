3b783be51145502188c3f1c9ed6f45023b3c190d
fernando <rluis4490@gmail.com>
2019-08-05 15:17:30 -0600

    create component HomePage in folder components/pages/hompage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './components/pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
