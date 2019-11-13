769352b67c6ece93864644f3975af32d8d90a5c1
fernando <rluis4490@gmail.com>
2019-11-12 23:19:21 -0600

    installed redux redux-logger and reaxt-redux

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>, 
-                    document.getElementById('root')
-                );
+ReactDOM.render(    
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
 
